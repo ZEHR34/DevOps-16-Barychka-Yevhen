0dfd783c0a7bc9433dc2491fb2d47a7020c8f31e
ZEHR34 <entropia.cfg@gmail.com>
2022-07-13 16:49:23 +0300

    python task 3 refactor

---
+++ m2-Python/Task-03/util.py
@@ -0,0 +1,50 @@
+import string
+from random import choice
+
+
+def getRandomChar(template: str) -> str:
+    k = 1
+    if len(template) > 1:
+        k = int(template[1:])
+    elif len(template) < 1:
+        return ""
+    type = template[0]
+    if type == "a":
+        return "".join([choice(string.ascii_lowercase) for i in range(k)])
+    elif type == "A":
+        return "".join([choice(string.ascii_uppercase) for i in range(k)])
+    elif type == "d":
+        return "".join([choice(string.digits) for i in range(k)])
+    elif type == "-":
+        return "".join(["-" for i in range(k)])
+    elif type == "@":
+        return "".join(["@" for i in range(k)])
+    else:
+        raise Exception("uncorrected template")
+
+
+def genFromTemplate(template: str) -> str:
+    template = unpackTemplate(template)
+    a = template.split("%")
+    try:
+        a.remove("")
+        a.remove("")
+        a.remove("")
+    except ValueError:
+        pass
+    password = [getRandomChar(i) for i in a]
+    return "".join(password)
+
+
+def unpackTemplate(template: str) -> str:
+    try:
+        begin = template.index("[")
+        end = template.index("]")
+    except ValueError:
+        return template
+    k = 1
+    if template[end+1].isnumeric():
+        k = int(template[end+1])
+    types = template[begin+1:end].split("%")
+    types.remove("")
+    return template[:begin] + "%".join([choice(types) for _ in range(k)])+template[end+2:]
