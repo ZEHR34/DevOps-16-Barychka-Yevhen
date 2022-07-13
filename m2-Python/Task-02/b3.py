24d375cafe6ca6274c47db79002d2dd1eda65327
ZEHR34 <entropia.cfg@gmail.com>
2022-07-13 12:28:24 +0300

    python task 2b 3

---
+++ m2-Python/Task-02/b3.py
@@ -0,0 +1,16 @@
+ip = list(map(int, input("ведіть ip адресу в форматі 10.0.1.1: ").split(".")))
+
+if len(ip) != 4 or len([i for i in ip if 0 <= i <= 255]) != 4:
+    print("Неправильный IP-адрес")
+    exit(1)
+
+if 1<=ip[0]<=223:
+    print("unicast")
+elif 224<=ip[0]<=239:
+    print("multicast")
+elif len([i for i in ip if i==255]) == len(ip):
+    print("local broadcast")
+elif len([i for i in ip if i==0]) == len(ip):
+    print("unassigned")
+else:
+    print("unused")
