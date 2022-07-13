d25e7ae411d7ca75576f589a3b1ce0233b231eeb
ZEHR34 <entropia.cfg@gmail.com>
2022-07-13 16:29:30 +0300

    python task 3 template generator

--- m2-Python/Task-03/passGenerator.py
+++ m2-Python/Task-03/passGenerator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import argparse
+import string
+from random import choice
 
 parser = argparse.ArgumentParser()  # description='Process some integers.')
 # parser.add_argument('integers', metavar='c', type=int, nargs='+',
@@ -7,7 +9,7 @@ parser = argparse.ArgumentParser()  # description='Process some integers.')
 parser.add_argument('-c', dest="count", default=1, type=int,
                     help='number of passwords')
 
-parser.add_argument('-l', dest="len", type=int,
+parser.add_argument('-l', dest="length", type=int,
                     help='set length of password ')
 
 parser.add_argument('-t', dest="template", type=str,
@@ -23,3 +25,67 @@ if args.file:
     print(args.file.readlines())
     args.file.close()
 
+#--------------------------
+if args.length:
+    for j in range(args.count):
+        s = [choice(string.digits+string.ascii_lowercase+string.ascii_uppercase) for i in range(args.length)]
+        print("".join(s))
+
+
+def getRandomChar(template: str) -> str:
+    k = 1
+    if len(template) > 1:
+        k = int(template[1:])
+    elif len(template) < 1:
+        return ""
+    type = template[0]
+    if type == "a":
+        return "".join([choice(string.ascii_lowercase) for i in range(k)])
+    elif type == "A":
+        return "".join([choice(string.ascii_uppercase) for i in range(k)])
+    elif type == "d":
+        return "".join([choice(string.digits) for i in range(k)])
+    elif type == "-":
+        return "".join(["-" for i in range(k)])
+    elif type == "@":
+        return "".join(["@" for i in range(k)])
+    else:
+        raise Exception("uncorect template")
+
+
+def genFromTemplate(template: str) -> str:
+    template = unpackTemplate(template)
+    a = template.split("%")
+    try:
+        a.remove("")
+        a.remove("")
+        a.remove("")
+    except ValueError:
+        pass
+    password = [getRandomChar(i) for i in a]
+    return "".join(password)
+
+
+def unpackTemplate(template: str) -> str:
+    try:
+        begin = template.index("[")
+        end = template.index("]")
+    except ValueError:
+        return template
+    k = 1
+    if template[end+1].isnumeric():
+        k = int(template[end+1])
+    types = template[begin+1:end].split("%")
+    types.remove("")
+    return template[:begin] + "%".join([choice(types) for _ in range(k)])+template[end+2:]
+
+
+
+if args.template:
+    print(genFromTemplate(args.template))
+
+# print("digits: "+string.digits)
+# print("punctuation: "+string.punctuation)
+# print("ascii_lowercase: "+string.ascii_lowercase)
+# print("ascii_uppercase: "+string.ascii_uppercase)
+# print(string.digits+string.ascii_lowercase+string.ascii_uppercase)
