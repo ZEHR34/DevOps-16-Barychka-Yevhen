ae2dd91c0217ca10abcdf6bf18e145644c0ed89e
ZEHR34 <entropia.cfg@gmail.com>
2022-08-02 22:20:48 +0300

    python task 4 csv export

--- m2-Python/Task-04/csvParser.py
+++ m2-Python/Task-04/csvParser.py
@@ -21,6 +21,8 @@ def in_range(diapason: [str], x: int) -> bool:
 
 def in_filter(row: dict, regulars:[str]) -> bool:
     """is dict not conflict with list of regulars: ['key=regular','key2=regular2']"""
+    if not regulars:
+        return True
     for i in regulars:
         fild, reg = i.split("=")
         if not fullmatch(reg, row[fild]):
@@ -42,13 +44,16 @@ parser.add_argument('-s', dest="field", nargs='+',
                     help='set field to use')
 
 parser.add_argument('-t', dest="reg", nargs='+',
-                    help='set regulars to field')
+                    help="set regulars to field -t user_name=User01 'IPv4=172.16.48.10\d'")
 
 parser.add_argument('-r', dest="range", nargs='+',
                     help='set lines 1,2,3 and/or 1-3')
 
-parser.add_argument('-f', dest="file", type=str,
+parser.add_argument('-f', dest="file", type=str, required=True,
                     help='csv file')
+
+parser.add_argument('-e', dest="export_file", type=str,
+                    help='csv file for export')
 
 # parser.add_argument('-v', dest="verbose", action='count', default=0)
 
@@ -64,13 +69,15 @@ args = parser.parse_args()
 file_name = args.file
 keys = args.field
 print(keys)
-
+# exit()
 
 # print(args.reg)
 
 
 with open(file_name) as csvfile:
     reader = csv.DictReader(csvfile, quotechar="'")
+    if not keys:
+        keys = reader.fieldnames
     b = reader.fieldnames
     a = []
     i = 0
@@ -78,6 +85,17 @@ with open(file_name) as csvfile:
         i += 1
         # print(row["user_name"] + str(in_filter(row, args.reg)))
         if in_range(args.range, i) and in_filter(row, args.reg):
-            a.append([row[i] for i in keys])
+            a.append([row[j] for j in keys])
 
 for i in a: print(i)
+data = [dict(zip(keys, i)) for i in a]
+print(data)
+print(keys)
+if args.export_file:
+    with open(args.export_file, "w") as file:
+        writer = csv.DictWriter(file, fieldnames=keys, quotechar="'")
+        writer.writeheader()
+        writer.writerows(data)
+
+# ./csvParser.py -s hostname IPv4 user_name -r 1-3 -f file.csv -t 'user_name=User0\d' 'IPv4=172.16.48.10[12]' -e test.csv
+
