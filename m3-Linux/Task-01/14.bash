ec77fa8c625c1dece8a7370e720fde6e5b2741c1
ZEHR34 <entropia.cfg@gmail.com>
2022-08-09 21:52:39 +0300

    Linux Task 1 part 18

--- m3-Linux/Task-01/14.bash
+++ m3-Linux/Task-01/14.bash
@@ -9,9 +9,21 @@ users
 # 16
 netstat -t
 
-#17
+# 17
 echo 123 > test.txt
 echo qwerty > test2.txt
 ln -s test.txt testlink.txt
 ln -sfn test2.txt testlink.txt
 rm test.txt test2.txt testlink.txt
+
+# 18
+mkdir "testf"
+pathf="./testf"
+echo 1234 > testf/1.txt
+echo qwer > testf/2.txt
+mkdir "testlfInks"
+for i in `ls $pathf`
+do
+  ln -s ../$pathf/$i testlfInks/$i
+done
+rm -r testlfInks testf -r
