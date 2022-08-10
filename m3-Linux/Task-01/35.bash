9976870670631974b0bae286afc3c7d8681ff067
ZEHR34 <entropia.cfg@gmail.com>
2022-08-10 17:17:59 +0300

    Linux Task 1 part 36

--- m3-Linux/Task-01/35.bash
+++ m3-Linux/Task-01/35.bash
@@ -4,7 +4,6 @@
 files=$(find img | grep .txt | awk -F/ '{print $(NF)}' | awk -F. '{print $1}')
 images=$(find img | grep .jpg)
 
-
 for i in $images;
 do
   name=$(echo $i | awk -F/ '{print $(NF)}' | awk -F. '{print $1}')
@@ -14,3 +13,8 @@ do
     echo $i haven\'t .txt file
   fi
 done
+
+# 36
+curl ifconfig.me
+echo
+hostname -I | awk '{print $1}'
