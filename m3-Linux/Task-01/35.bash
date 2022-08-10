eb7a054681097aea24b8f5397d69edf2ed2cf5c1
ZEHR34 <entropia.cfg@gmail.com>
2022-08-10 17:15:10 +0300

    Linux Task 1 part 35

---
+++ m3-Linux/Task-01/35.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 35
+files=$(find img | grep .txt | awk -F/ '{print $(NF)}' | awk -F. '{print $1}')
+images=$(find img | grep .jpg)
+
+
+for i in $images;
+do
+  name=$(echo $i | awk -F/ '{print $(NF)}' | awk -F. '{print $1}')
+  if [[ " ${files} " =~ " ${name} " ]]; then
+    echo $i have .txt file
+  else
+    echo $i haven\'t .txt file
+  fi
+done
