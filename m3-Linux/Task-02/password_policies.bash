3512f39128144f7caaef99b94a3a3f13ac3559ef
Your Name <you@example.com>
2022-08-23 11:04:11 +0300

    password policies update

--- m3-Linux/Task-02/password_policies.bash
+++ m3-Linux/Task-02/password_policies.bash
@@ -1,14 +1,14 @@
 #/usr/bin/bash
 
 OLD='pam_unix.so obscure use_authtok try_first_pass yescrypt'
-NEW="$OLD minlen=8 dcredit=-1 ocredit=-1 minclass=2"
+NEW="$OLD minlen=8 dcredit=-1 ocredit=-1 minclass=2 remember=3"
 FILE='/etc/pam.d/common-password'
 
-echo $NEW
 
-if [ "$1" == "b"  ] || [ $# -gt 1  ]; then
-          echo "Parameter 1 is empty"
-          sed -i -e "s/$NEW/$OLD/g" "$FILE"
-else
-          sed -i -e "s/$OLD\$/$NEW/g" "$FILE"
-fi
+sed -i -e "s/$OLD\$/$NEW/g" "$FILE"
+
+
+FILE2='/etc/login.defs'
+
+sed -i -e "s/PASS_MAX_DAYS\t99999/PASS_MAX_DAYS\t90/g" "$FILE2"
+sed -i -e "s/PASS_WARN_AGE\t7/PASS_WARN_AGE\t0/g" "$FILE2"
