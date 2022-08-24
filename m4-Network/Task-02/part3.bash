effa1f4209375b2d78c9b3e427dfe43cc1771450
Your Name <you@example.com>
2022-08-24 16:34:28 +0300

    m 4 task 2 part 3

---
+++ m4-Network/Task-02/part3.bash
@@ -0,0 +1,7 @@
+curl -v httpstat.us/206 -H test:test -H test2:test2
+curl -v httpstat.us/207 -H test:test -H test2:test2
+curl -v httpstat.us/208 -H test:test -H test2:test2
+curl -v httpstat.us/209 -H test:test -H test2:test2
+curl -v httpstat.us/210 -H test:test -H test2:test2
+curl -v httpstat.us/211 -H test:test -H test2:test2
+curl -v httpstat.us/212 -H test:test -H test2:test2
