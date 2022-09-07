2098db56097c423fe1f04072f2a905538768932a
ZEHR34 <entropia.cfg@gmail.com>
2022-09-07 17:54:40 +0300

    python task 5 part B item 3

--- m2-Python/Task-05/main.py
+++ m2-Python/Task-05/main.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
 import re
 
-ips=dict()
+def dict_incrase(dictc: dict,kay: str):
+    try: dictc[kay]+=1
+    except KeyError: dictc[kay]=1
+
+def print_top(dict: dict, n: int):
+    top=list(dict.items())
+    top.sort(key=lambda x:-x[1])
+    for i in range(n):
+        print(f'{top[i][0]}\t{top[i][1]}')
+
+
 f=open("access_log","r")
+
+print()
+ips=dict()
 for i in f.readlines():
     for j in re.findall('(\d+\.\d+\.\d+\.\d+)', i):
-        try: ips[j]+=1
-        except KeyError:
-            ips[j]=1
-ipsansver=list(ips.items())
-ipsansver.sort(key=lambda x:-x[1])
-N=5
-for i,j in ipsansver:
-    print(f"{i}\t{j}")
-    if N<=0:
-        break
-    N-=1
+        dict_incrase(ips,j)
+print_top(ips,5)
+f.seek(0)
+
+print()
+clients=dict()
+for i in f.readlines():
+    j = re.findall(r'\"[A-Za-z0-9,;\(\)_/ \\.:\?=&+%#\-]*\"', i)
+    agent = j[-2].split()[0].replace('"',"")
+    dict_incrase(clients,agent)
+
+print_top(clients,5)
+f.seek(0)
 
 
